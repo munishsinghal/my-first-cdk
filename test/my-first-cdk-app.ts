332104eb95f5b7fbf50a949127b79a9771409e33
Munish <munishsinghal@gmail.com>
2024-09-08 06:35:52 +1000

    added new ci

---
+++ test/my-first-cdk-app.ts
@@ -0,0 +1,13 @@
+import { handler } from '../src'
+
+describe('My First CDK App Tests', () => {
+    it("returns name and list", async()=>{
+        const output = await handler({name: 'Faruk'});
+        expect(output).toStrictEqual('Good Job Munish');
+    })
+
+    it("returns no name and list", async()=>{
+        const output = await handler({name: ''});
+        expect(output).toStrictEqual('Good job without event');
+    })
+});
