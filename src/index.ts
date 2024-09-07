66af125a38beb15536929eee74523aaa12a5863c
Munish <munishsinghal@gmail.com>
2024-09-08 07:17:07 +1000

    2nd fix

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export const handler = async (event: { name: string }) => {
-    const result: string = event.name ? `Good Job ${event.name}` : 'Good job without event'
+    const result: string = event.name ? `Good Job ${event.name}` : 'Good job without event1'
     return result;
 }
